8c883d610a7ab5f53d0a0e1420d4c89ccd9a6579
yanxing <qwe33370@gmail.com>
2025-07-20 19:08:24 +0800

    add fake student_names (fake datas) ,add  draw service and test on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:lottery_app/services/draw_service.dart';
+
+// void main() {
+//   runApp(const MaterialApp(home: TestDrawPage()));
+// }
+//
+// class TestDrawPage extends StatefulWidget {
+//   const TestDrawPage({super.key});
+//
+//   @override
+//   State<TestDrawPage> createState() => _TestDrawPageState();
+// }
+//
+// class _TestDrawPageState extends State<TestDrawPage> {
+//   final ChoosingStudent chooser = ChoosingStudent();
+//
+//   String? chosen;
+//   List<String> remaining = [];
+//
+//   void draw() {
+//     setState(() {
+//       chooser.chooseStudent();
+//       chosen = chooser.showStudent();
+//       chooser.deleteStudent();
+//       remaining = chooser.getRemaining();
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('抽籤測試')),
+//       body: Padding(
+//         padding: const EdgeInsets.all(20.0),
+//         child: Column(
+//           children: [
+//             ElevatedButton(
+//               onPressed: draw,
+//               child: const Text('抽籤'),
+//             ),
+//             const SizedBox(height: 20),
+//             Text('抽到：${chosen ?? "尚未抽"}'),
+//             const SizedBox(height: 20),
+//             Text('剩下：$remaining'),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+import 'package:flutter/material.dart';
+import 'package:lottery_app/services/draw_service.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +68,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -23,16 +77,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,38 +88,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
