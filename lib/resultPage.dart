25460825e0062aa4964b3d89177815143d7603de
Test <mikeof122@gmail.com>
2025-07-21 14:34:54 +0800

    finished animations and basic draw

--- lib/resultPage.dart
+++ lib/resultPage.dart
@@ -1,1 +1,27 @@
 import 'package:flutter/material.dart';
+
+class ResultPage extends StatelessWidget {
+  const ResultPage({
+    super.key,
+    required this.name,
+  });
+
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Result'),
+      ),
+      body: Center(
+        child: Stack(
+          children: [
+            // Image(image: ),
+            Text(name),
+          ],
+        ),
+      ),
+    );
+  }
+}
