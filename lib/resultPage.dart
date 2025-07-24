c368240978a94813810500d68ee5e0f847a7d96d
Test <mikeof122@gmail.com>
2025-07-24 11:06:21 +0800

    add special effect on resultPage

--- lib/resultPage.dart
+++ lib/resultPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-class ResultPage extends StatelessWidget {
+class ResultPage extends StatefulWidget {
   const ResultPage({
     super.key,
     required this.name,
@@ -9,19 +10,42 @@ class ResultPage extends StatelessWidget {
   final String name;
 
   @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Result'),
       ),
       body: Center(
-        child: Stack(
+        child: Column(
           children: [
-            // Image(image: ),
-            Text(name, style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
-            )),
+            Stack(
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    // Image(image: ),
+                    FittedBox(
+                      child: Text(widget.name, style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                      )),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height - 56,
+                      width: double.infinity,
+                      child: Lottie.network(
+                        'https://lottie.host/216adf2c-75a4-420c-8986-bfbf12e806ec/VHW7qTjeRi.json',
+                      ),
+                    ),
+                  ],
+                ),
+              ]
+            ),
           ],
         ),
       ),
